07593f4a36c27ac9444523747e7b6a47d21105ac
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-14 15:05:09 +0500

    Add new tables and alter animals table to add foreign key

--- schema.sql
+++ schema.sql
@@ -14,4 +14,28 @@ CREATE TABLE animals (
 
 -- Project 02
 
-ALTER TABLE animals ADD species varchar(100); 
+ALTER TABLE animals ADD species varchar(100);
+
+-- Project 03
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(250),
+    age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_animal_species
+FOREIGN KEY (species_id)
+REFERENCES species(id);
