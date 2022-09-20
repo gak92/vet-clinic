cb559cd17ee03819175f8645aff586e4e6227c14
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-20 17:50:08 +0500

    Add medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,10 +4,18 @@ CREATE DATABASE clinic;
 -- Create table patients
 CREATE TABLE patients (
   id INT GENERATED ALWAYS AS IDENTITY,
-  name varchar(100),
+  name VARCHAR(100),
   date_of_birth DATE,
   PRIMARY KEY (id)
 );
 
-
+-- Create table medical histories
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP,
+  status VARCHAR(250),
+  patient_id INT,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_patient_id FOREIGN KEY patient_id REFERENCES patients(id)
+);
 
